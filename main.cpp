dd15352379532048b576d1a4a783f32de2251a66
Another-Person <manjoey7@gmail.com>
2020-11-30 19:30:51 -0600

    Prints the raw CPU idle, kernel, and user times

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 ﻿#include "pch.h"
 
+#include <iostream>
+
 using namespace winrt;
 using namespace Windows::Foundation;
+using namespace Windows::System::Diagnostics;
 
 int main()
 {
     init_apartment();
-    Uri uri(L"http://aka.ms/cppwinrt");
-    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+    SystemDiagnosticInfo diagnosticInfo = SystemDiagnosticInfo::GetForCurrentSystem();
+
+    TimeSpan cpuIdleTime{diagnosticInfo.CpuUsage().GetReport().IdleTime()};
+    std::wcout << "The idle CPU time was:   " << cpuIdleTime.count() << "\n";
+
+    TimeSpan cpuKernelTime{diagnosticInfo.CpuUsage().GetReport().KernelTime()};
+    std::wcout << "The CPU kernel time was: " << cpuKernelTime.count() << "\n";
+
+    TimeSpan cpuUserTime{diagnosticInfo.CpuUsage().GetReport().UserTime()};
+    std::wcout << "The CPU user time was:   " << cpuUserTime.count() << "\n";
 }
