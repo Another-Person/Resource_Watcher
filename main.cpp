2aea0c668b05bb084c7099d0b80617f9f93dd49a
Another-Person <manjoey7@gmail.com>
2020-11-30 19:54:59 -0600

    Now repeats on a loop every 3 seconds

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 ﻿#include "pch.h"
 
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 using namespace winrt;
 using namespace Windows::Foundation;
@@ -8,19 +10,29 @@ using namespace Windows::System::Diagnostics;
 
 int main()
 {
-    init_apartment();
-    SystemDiagnosticInfo diagnosticInfo = SystemDiagnosticInfo::GetForCurrentSystem();
+  init_apartment();
 
-    TimeSpan cpuIdleTime{diagnosticInfo.CpuUsage().GetReport().IdleTime()};
-    std::wcout << "The idle CPU time was:   " << cpuIdleTime.count() << "\n";
+  while (true) 
+  {
+      SystemDiagnosticInfo diagnosticInfo = SystemDiagnosticInfo::GetForCurrentSystem();
 
-    TimeSpan cpuKernelTime{diagnosticInfo.CpuUsage().GetReport().KernelTime()};
-    std::wcout << "The CPU kernel time was: " << cpuKernelTime.count() << "\n";
+      TimeSpan cpuIdleTime{diagnosticInfo.CpuUsage().GetReport().IdleTime()};
+      std::wcout << "The idle CPU time was:   " << cpuIdleTime.count() << "\n";
 
-    TimeSpan cpuUserTime{diagnosticInfo.CpuUsage().GetReport().UserTime()};
-    std::wcout << "The CPU user time was:   " << cpuUserTime.count() << "\n";
+      TimeSpan cpuKernelTime{diagnosticInfo.CpuUsage().GetReport().KernelTime()};
+      std::wcout << "The CPU kernel time was: " << cpuKernelTime.count() << "\n";
 
-    std::wcout << "The available memory in bytes was: " << diagnosticInfo.MemoryUsage().GetReport().AvailableSizeInBytes() << "\n";
-    std::wcout << "The used memory in bytes was:      " << diagnosticInfo.MemoryUsage().GetReport().CommittedSizeInBytes() << "\n";
-    std::wcout << "The total memory in the system is: " << diagnosticInfo.MemoryUsage().GetReport().TotalPhysicalSizeInBytes() << "\n";
+      TimeSpan cpuUserTime{diagnosticInfo.CpuUsage().GetReport().UserTime()};
+      std::wcout << "The CPU user time was:   " << cpuUserTime.count() << "\n";
+
+      std::wcout << "The available memory in bytes was: " << diagnosticInfo.MemoryUsage().GetReport().AvailableSizeInBytes() << "\n";
+      std::wcout << "The used memory in bytes was:      " << diagnosticInfo.MemoryUsage().GetReport().CommittedSizeInBytes() << "\n";
+      std::wcout << "The total memory in the system is: " << diagnosticInfo.MemoryUsage().GetReport().TotalPhysicalSizeInBytes() << "\n";
+
+      std::wcout << "\n";
+      std::wcout << "---\n";
+      std::wcout << "\n";
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+  }
 }
