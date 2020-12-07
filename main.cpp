52a1a4fb5ec623eb53c52e879184eb557d835ff9
Another-Person <manjoey7@gmail.com>
2020-12-07 16:08:19 -0600

    Formatting updates to output and code

--- main.cpp
+++ main.cpp
@@ -15,18 +15,19 @@ int main()
 	while (true)
 	{
 		SystemDiagnosticInfo diagnosticInfo = SystemDiagnosticInfo::GetForCurrentSystem();
-
 		SystemCpuUsageReport cpuUsageReport = diagnosticInfo.CpuUsage().GetReport();
 
+		// Find total CPU time, then create percentages
 		long long totalUsageTime = cpuUsageReport.IdleTime().count() + cpuUsageReport.KernelTime().count() + cpuUsageReport.UserTime().count();
-		double percentIdleTime = static_cast<double>(cpuUsageReport.IdleTime().count()) / totalUsageTime;
-		double percentKernelTime = static_cast<double>(cpuUsageReport.KernelTime().count()) / totalUsageTime;
-		double percentUserTime = static_cast<double>(cpuUsageReport.UserTime().count()) / totalUsageTime;
+		double percentIdleTime = (static_cast<double>(cpuUsageReport.IdleTime().count()) / totalUsageTime) * 100;
+		double percentKernelTime = (static_cast<double>(cpuUsageReport.KernelTime().count()) / totalUsageTime) * 100;
+		double percentUserTime = (static_cast<double>(cpuUsageReport.UserTime().count()) / totalUsageTime) * 100;
 
-		std::wcout << "Percent of CPU time that was idle:   " << percentIdleTime << "% \n";
-		std::wcout << "Percent of CPU time in kernel mode: " << percentKernelTime << "% \n";
-		std::wcout << "Percent of CPU time in user mode:   " << percentUserTime << "% \n";
-
+		std::wcout << "Percent of CPU time idling:          " << percentIdleTime << "% \n";
+		std::wcout << "Percent of CPU time in kernel mode:  " << percentKernelTime << "% \n";
+		std::wcout << "Percent of CPU time in user mode:    " << percentUserTime << "% \n";
+		std::wcout << "\n";
+		
 		SystemMemoryUsageReport memoryUsageReport = diagnosticInfo.MemoryUsage().GetReport();
 
 		// Divide count in bytes by definition of MiB
@@ -36,15 +37,13 @@ int main()
 		long long committedMemoryInMiB = memoryUsageReport.CommittedSizeInBytes() / MiB_Definition;
 		long long totalMemoryInMiB = memoryUsageReport.TotalPhysicalSizeInBytes() / MiB_Definition;
 
-		std::wcout << "The available memory was: " << freeMemoryInMiB << " MiB \n";
-		std::wcout << "The used memory was:      " << usedMemoryInMiB << " MiB \n";
-		std::wcout << "The total committed memory was:    " << committedMemoryInMiB << " MiB \n";
-		std::wcout << "The total memory in the system is: " << totalMemoryInMiB << " MiB\n";
-
+		std::wcout << "The available memory was:            " << freeMemoryInMiB << " MiB \n";
+		std::wcout << "The used memory was:                 " << usedMemoryInMiB << " MiB \n";
+		std::wcout << "The total committed memory was:      " << committedMemoryInMiB << " MiB \n";
+		std::wcout << "The total memory in the system is:   " << totalMemoryInMiB << " MiB\n";
 		std::wcout << "\n";
 		std::wcout << "---\n";
 		std::wcout << "\n";
-
 		std::this_thread::sleep_for(std::chrono::seconds(3));
 	}
 }
